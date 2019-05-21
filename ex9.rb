89e625445ece960780f9e30fc4556f981ebdc2f9
Leo  Moon <munguial@unam.mx>
2019-05-20 20:38:07 -0500

    Ejemplos 7 al 10

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+# Here's some new strange stuff, remember type it exactly.
+#
+
+days= "Lun Mar Mie Jue Vie Sab Dom"
+months=  "Ene\nFeb\nMar\nAbr\nMay\nJun\nJul\nAgo\nSep\nOct\nNov\nDic"
+
+puts " Aqui los dias : #{days}"
+puts " Aqui los meses : #{months} "
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
